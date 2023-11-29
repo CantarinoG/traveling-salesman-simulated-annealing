b2e9cdf13b55e55fdc9f45c2755be1e9217c749e
CantarinoG <guilhermecantarino1709@gmail.com>
2023-11-29 17:43:31 -0300

    Add function to generate initial solution

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ function criarCoordenadas(n) { //Esta função gera uma lista com n pares de coo
     return lista;
 }
 
+function criarSolucaoInicial(n) { //Cria uma solução inicial
+    let solucaoInicial = []
+    for (let i = 0; i < n; i++) {
+        solucaoInicial.push(i);
+    }
+    for (let i = 0; i < n; i++) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [solucaoInicial[i], solucaoInicial[j]] = [solucaoInicial[j], solucaoInicial[i]];
+    }
+    return solucaoInicial
+}
+
 let numeroCidades = 10
 let cidades = criarCoordenadas(numeroCidades) //Coordenadas geradas aleatoriamente para o nosso problema
+let solucaoInicial = criarSolucaoInicial(numeroCidades) 
