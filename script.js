00966f13cca2a889d57614cbf494529b73dbc74d
CantarinoG <guilhermecantarino1709@gmail.com>
2023-11-29 19:59:12 -0300

    Add html page structure

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function criarCoordenadas(n) { //Esta função gera uma lista com n pares de coordenadas que variam entre 0 e 100
+function criarCoordenadas(n) { //Esta função gera uma lista com n pares de coordenadas que variam entre 0 e 400
     let lista = [];
 
     for (let i = 0; i < n; i++) {
-        let coordenada = [Math.floor(Math.random() * 101), Math.floor(Math.random() * 101)];
+        let coordenada = [Math.floor(Math.random() * 401), Math.floor(Math.random() * 401)];
         lista.push(coordenada);
     }
 
